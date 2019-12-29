604b5fa9e944169ad8dbdeaee50ecd6ff3399ba6
xiningz <rachelzxn1@gmail.com>
2019-12-28 21:24:45 -0800

    implement CRUD

--- Ins/urls.py
+++ Ins/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from Ins.views import HelloWorld
+from Ins.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld')
+    path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/',PostsView.as_view(), name='posts'),
+    path('posts/<int:pk>',PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/',PostCreateView.as_view(), name='make_post'),
+    path('posts/update/<int:pk>',PostUpdateView.as_view(), name='post_update'),
+    path('posts/delete/<int:pk>',PostDeleteView.as_view(), name='post_delete'),
 ]
